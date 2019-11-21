732566b10ef3a804361f3b1aef316fffdacae10b
esiji <esiji2210@gmail.com>
2019-11-21 19:23:30 +0100

    last

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,18 +11,18 @@ class Contact extends React.Component {
     return (
       <section className="contact">
         <h2 className="contact-title">Contact me</h2>
-        <form className="contact-form" action="./contact.php" method="POST">
+        <form className="contact-form" action="contact.php" method="POST">
             <div className="contact-container">
                 <label className="contact-label" htmlFor="email">Email</label>
-                <input className="contact-input" id="email" name="email" type="email" placeholder="Email"></input>
+                <input className="contact-input" id="email" name="email" type="email" placeholder="Email" required></input>
             </div>
             <div className="contact-container">
                 <label className="contact-label" htmlFor="name">Name</label>
-                <input className="contact-input" id="name" name="name" type="text" placeholder="Name"></input>
+                <input className="contact-input" id="name" name="name" type="text" placeholder="Name" required></input>
             </div>
             <div className="contact-container">
                 <label className="contact-label" htmlFor="message">Message</label>
-                <textarea className="contact-textarea" id="message" name="message" rows="8" placeholder="Your message"></textarea>
+                <textarea className="contact-textarea" id="message" name="message" rows="8" placeholder="Your message" required></textarea>
             </div>
             <div className="contact-container">
                 <input className="contact-submit" id="submit" name="submit" type="submit" value="Send" />
--- src/Components/contact.php
+++ src/Components/contact.php
@@ -1,14 +1,10 @@
 <?php
 
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
+$name = $_POST['name'];
+$mailFrom = $_POST['email'];
+$message = $_POST['message'];
+$mailTo = "luki250350@yahoo.com";
+$headers = "From: ".$mailFrom;
+mail($mailTo, $name, $message, $headers);
 
-    $mailTo = "luki250350@yahoo.com";
-    $headers = "From: ".$mailFrom;
-
-    mail($mailTo, $name, $message, $headers);
-    header("Location: index.html?mailsend")
-}
 ?>
