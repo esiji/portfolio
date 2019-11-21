bb1e494396a8eea9dc31d253ad4d46aa0f9ce72f
esiji <esiji2210@gmail.com>
2019-11-21 19:53:15 +0100

    lol

--- src/Components/contact.php
+++ src/Components/contact.php
@@ -1,14 +1,16 @@
 <?php
 
-if($_POST) {
+if(isset($_POST['submit'])){
     $name = $_POST['email_name'];
     $mailFrom = $_POST['email_email'];
     $message = $_POST['email_message'];
-    $mailTo = "luki250350@yahoo.com";
+
+    $mailTo = "luki250350@gmail.com";
     $headers = "From: ".$mailFrom;
+
     mail($mailTo, $name, $message, $headers);
 
-    echo "thank you."
+    echo("Message sent :)")
 }
 
-?>  
+?>
