6d2315f58db452822a64b3c02bbb6268e5d3016b
esiji <esiji2210@gmail.com>
2019-11-22 15:26:33 +0100

    last

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,18 +11,18 @@ class Contact extends React.Component {
     return (
       <section className="contact">
         <h2 className="contact-title">Contact me</h2>
-        <form className="contact-form" action="contact.php" method="POST">
+        <form className="contact-form" action="https://formspree.io/mrggwpve" method="POST">
             <div className="contact-container">
                 <label className="contact-label" htmlFor="email">Email</label>
-                <input className="contact-input" id="email" name="email_email" type="email" placeholder="Email" required></input>
+                <input className="contact-input" id="email" name="email" type="email" placeholder="Email" required></input>
             </div>
             <div className="contact-container">
                 <label className="contact-label" htmlFor="name">Name</label>
-                <input className="contact-input" id="name" name="email_name" type="text" placeholder="Name" required></input>
+                <input className="contact-input" id="name" name="name" type="text" placeholder="Name" required></input>
             </div>
             <div className="contact-container">
                 <label className="contact-label" htmlFor="message">Message</label>
-                <textarea className="contact-textarea" id="message" name="email_message" rows="8" placeholder="Your message" required></textarea>
+                <textarea className="contact-textarea" id="message" name="message" rows="8" placeholder="Your message" required></textarea>
             </div>
             <div className="contact-container">
                 <input className="contact-submit" id="submit" name="submit" type="submit" value="Send" />
